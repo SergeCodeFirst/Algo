6b4a1eceace7a76881c3aa7076b45343588755ff
Itserge1 <sergecodefirst@gmail.com>
2023-08-09 10:40:31 -0400

    two sum 2 update

--- NeetCode_TwoSum_II_167.py
+++ NeetCode_TwoSum_II_167.py
@@ -28,9 +28,9 @@ class Solution:
         while l < r:
             if numbers[l] + numbers[r] > target:
                 r -= 1
-            if numbers[l] + numbers[r] < target:
+            elif numbers[l] + numbers[r] < target:
                 l += 1
-            if numbers[l] + numbers[r] == target:
+            elif numbers[l] + numbers[r] == target:
                 return [l+1, r+1]
     
         return [-1, -1]
