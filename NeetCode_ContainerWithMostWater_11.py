1cb05a8b212497eb340600cf1686b9eeba7aca45
Itserge1 <sergecodefirst@gmail.com>
2023-08-24 18:53:48 -0400

    Container with most Water

---
+++ NeetCode_ContainerWithMostWater_11.py
@@ -0,0 +1,45 @@
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+# Return the maximum amount of water a container can store.
+# Notice that you may not slant the container.
+
+
+# Example 1:
+# Input: height = [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+# Example 2:
+# Input: height = [1,1]
+# Output: 1
+
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        MaxArea = 0
+        # for i in range(len(height)):
+        #     for j in range(i+1, len(height)):
+        #         MaxArea = max(min(height[i], height[j]) * (j-i) , MaxArea)
+
+        # return MaxArea
+
+        i = 0
+        j = len(height) -1
+
+        while i < j:
+            MaxArea = max(min(height[i], height[j]) * (j-i) , MaxArea)
+
+            if height[i] < height[j]:
+                i += 1
+            
+            else:
+                j -= 1
+            # if height[j] <=  height[i]:
+            #     j -= 1
+        
+        return MaxArea
+
+newSolution = Solution()
+
+print(newSolution.maxArea([1,8,6,2,5,4,8,3,7]))
