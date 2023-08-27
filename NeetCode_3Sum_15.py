c8e0058fb06895ae4fdb09c126e26de90ee31eef
Itserge1 <sergecodefirst@gmail.com>
2023-08-27 17:08:11 -0400

    Revise 3sum

--- NeetCode_3Sum_15.py
+++ NeetCode_3Sum_15.py
@@ -26,25 +26,56 @@ from typing import List
 
 
 class Solution:
+    # def threeSum(self, nums: List[int]) -> List[List[int]]:
+    #     res = []
+    #     nums.sort
+    #     for i, a in enumerate(nums):
+    #         if i > 0 and a == nums [i - 1]:
+    #             continue
+            
+    #         l, r = i + 1, len(nums) - 1
+    #         while 1 < r:
+    #             threeSum = a + nums[l] + nums[r]
+    #             if threeSum > 0:
+    #                 r -= 1 
+    #             elif threeSum < 0:
+    #                 l += 1 
+    #             else:
+    #                 res.append ([a, nums [l], nums [r]])
+    #                 l += 1
+    #                 while nums [l] == nums [l - 1] and l < r:
+    #                     l += 1
+    #     return res
+    
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-        nums.sort
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums [i - 1]:
-                continue
+        nums.sort()
+
+        for i, num in enumerate(nums):
+            if num > 0:
+                break
             
-            l, r = i + 1, len(nums) - 1
-            while 1 < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1 
-                elif threeSum < 0:
-                    l += 1 
-                else:
-                    res.append ([a, nums [1], nums [r]])
+            if i > 0 and num == nums[i - 1]:
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                if num + nums[l] + nums[r] == 0:
+                    res.append([num, nums[l], nums[r]])
                     l += 1
-                while nums [l] == nums [l - 1] and 1 < r:
+                    r -= 1
+                    while nums[l] == nums[l -1] and l < r:
+                        l+=1
+                
+                elif num + nums[l] + nums[r] > 0:
+                    r -= 1
+                
+                elif num + nums[l] + nums[r] < 0:
                     l += 1
+                
+                
         return res
 
 
--- a_train.py
+++ a_train.py
@@ -1,26 +1,67 @@
-
-from typing import List;
-# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
-# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-# You must write an algorithm that runs in O(n) time and without using the division operation.
-
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that 
+# i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+# Notice that the solution set must not contain duplicate triplets.
 
 # Example 1:
-# Input: nums = [1,2,3,4]
-# Output: [24,12,8,6]
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+
+# Explanation: 
+# nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+# nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+# nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+# The distinct triplets are [-1,0,1] and [-1,-1,2].
+# Notice that the order of the output and the order of the triplets does not matter.
 
 # Example 2:
-# Input: nums = [-1,1,0,-3,3]
-# Output: [0,0,9,0,0]
+# Input: nums = [0,1,1]
+# Output: []
+# Explanation: The only possible triplet does not sum up to 0.
+
+# Example 3:
+# Input: nums = [0,0,0]
+# Output: [[0,0,0]]
+# Explanation: The only possible triplet sums up to 0.
+
+# [-4,-1,-1,0,1,2]
+
+from typing import List
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        return []
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i, num in enumerate(nums):
+            if num > 0:
+                break
+            
+            if i > 0 and num == nums[i - 1]:
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                if num + nums[l] + nums[r] == 0:
+                    res.append([num, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l -1] and l < r:
+                        l+=1
+                
+                elif num + nums[l] + nums[r] > 0:
+                    r -= 1
+                
+                elif num + nums[l] + nums[r] < 0:
+                    l += 1
+                
+                
+        return res
+    
 
 newSolution = Solution()
 
-print(newSolution.productExceptSelf([2,2,3,1,1,1], k = 2))
-
-# Input: nums = [1,2,3,4]
-# Output: [0*1,1*2,1*2*3,1*2*3*4]
-# Output: [1*2*3*4,2*3*4,4*3,0*4]
+print(newSolution.threeSum([-2,0,0,2,2]))
+# print(newSolution.threeSum([-1,0,1,2,-1,-4]))
+# print(newSolution.threeSum([0,1,1]))
+# print(newSolution.threeSum([0,0,0]))
