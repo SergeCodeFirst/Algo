9d7df21e856f59f120536120603f006f0d3d5950
Itserge1 <sergecodefirst@gmail.com>
2023-08-29 12:59:08 -0400

    revise top k element

--- NeetCode_topKFrequentElements.py
+++ NeetCode_topKFrequentElements.py
@@ -1,5 +1,4 @@
 from typing import List
-from collections import defaultdict
 
 # Given an integer array nums and an integer k, 
 # return the k most frequent elements. You may return the answer in any order.
@@ -11,45 +10,69 @@ from collections import defaultdict
 # Output: [1]
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
-        
-        # freq = []
-        # for i in range(len(nums) + 1):
-        #     freq.append([])
-
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
-        for n, c in count.items():
-            freq[c].append(n)
-
-        res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
-
     # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-    #     # Create hasmap make a key from the integer in the array
-    #     newMap = defaultdict(int)
-    #     res =[]
-    #     # Increment everytime you get on a key
-    #     for num in nums:
-    #         newMap[num] += 1
-        
-    #     # Sort the keys based on their frequency in descending order
-    #     allKeys = sorted(newMap.keys(), key=lambda x: newMap[x], reverse=True)
+    #     # create a map num - count
+    #     num_Count = {}
+    #     res = []
 
-    #     for key in allKeys:
+    #     # Populate the map
+    #     for num in nums:
+    #         num_Count[num] = num_Count.get(num, 0) + 1
+        
+    #     # Sorting the map base of the count value of each key.
+    #     sortedMap = sorted(num_Count.items(), key=lambda item: item[1], reverse=True)
+
+    #     # get the most k frequent element
+    #     for key, val in sortedMap:
     #         if len(res) == k:
     #             break
     #         res.append(key)
     #     return res
     
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # create map
+        new_Map = {}
+        res = []
 
+        freq1 = [[] for i in range(len(nums) + 1)] # list comprehension create multiple distinct instance of list (when one is updated, it does not automaticaly update the other)
+        # In this case, you're correctly creating a list of distinct empty lists using a list comprehension. 
+        # Each inner list is a separate object, so appending elements to one inner list doesn't affect the others.
+        
+        # freq2 = [[]] * (len(nums) + 1) # create multiple unique instance of list (when one is updated, it automaticaly update the other)
+        # # you're creating a list of empty lists, but all of these inner lists are the same object. 
+        # # So, when you append an element to one of these inner lists, the change is reflected in all of them
+        
+        for num in nums:
+            new_Map[num] = new_Map.get(num, 0) + 1
 
-newSolution = Solution()
-print(newSolution.topKFrequent([1,1,1,2,2,3], 2))
-# print(newSolution.topKFrequent([1], 1))
+        for key, val in new_Map.items():
+            freq1[val].append(key)
+            # freq2[val].append(key)
+        # print(freq2)
+
+        for i in range(len(freq1) - 1, 0, -1):
+            for num in freq1[i]:
+                if len(res) == k:
+                    break
+                res.append(num)
+        
+        return res
+    
+NewSolution = Solution()
+print(NewSolution.topKFrequent([1,1,1,2,2,3], 2))
+
+# QUESTION
+# when i create freq like this 
+# freq1 = [[]] * (len(nums) + 1)
+# it looks like this [[], [], [], [], [], [], []]
+# the same thing goes for 
+# freq2 = [[] for i in range(len(nums) + 1)]
+# it looks like this [[], [], [], [], [], [], []]
+
+# for key, val in new_Map.items():
+#     freq1[val].append(key)
+#     freq2[val].append(key)
+
+# freq1 looks like :[[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]]
+# and freq2 look like: [[], [3], [2], [1], [], [], []]
+# Why?
--- a_train.py
+++ a_train.py
@@ -1,42 +1,78 @@
-# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
-# Find two lines that together with the x-axis form a container, such that the container contains the most water.
-# Return the maximum amount of water a container can store.
-# Notice that you may not slant the container.
+# Given an integer array nums and an integer k, 
+# return the k most frequent elements. You may return the answer in any order.
 
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
 
-# Example 1:
-# Input: height = [1,8,6,2,5,4,8,3,7]
-# Output: 49
-# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+# Input: nums = [1], k = 1
+# Output: [1]
 
-# Example 2:
-# Input: height = [1,1]
-# Output: 1
-
-from typing import List
+from typing import List;
 
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        l = 0
-        r = len(height) - 1 
-        maxArea = 0
+    # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    #     # create a map num - count
+    #     num_Count = {}
+    #     res = []
 
-        while l < r:
-            area = min(height[l], height[r])  * (r - l)
+    #     # Populate the map
+    #     for num in nums:
+    #         num_Count[num] = num_Count.get(num, 0) + 1
+        
+    #     # Sorting the map base of the count value of each key.
+    #     sortedMap = sorted(num_Count.items(), key=lambda item: item[1], reverse=True)
 
-            if area > maxArea:
-                maxArea = area
-            
-            # maxArea = max(min(height[l], height[r])  * (r - l), maxArea)
-            
-            if height[l] < height[r]:
-                l += 1
-            
-            else:
-                r -= 1
-
-        return maxArea
+    #     # get the most k frequent element
+    #     for key, val in sortedMap:
+    #         if len(res) == k:
+    #             break
+    #         res.append(key)
+    #     return res
     
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # create map
+        new_Map = {}
+        res = []
 
+        freq1 = [[] for i in range(len(nums) + 1)] # list comprehension create multiple distinct instance of list (when one is updated, it does not automaticaly update the other)
+        # In this case, you're correctly creating a list of distinct empty lists using a list comprehension. 
+        # Each inner list is a separate object, so appending elements to one inner list doesn't affect the others.
+        
+        # freq2 = [[]] * (len(nums) + 1) # create multiple unique instance of list (when one is updated, it automaticaly update the other)
+        # # you're creating a list of empty lists, but all of these inner lists are the same object. 
+        # # So, when you append an element to one of these inner lists, the change is reflected in all of them
+        
+        for num in nums:
+            new_Map[num] = new_Map.get(num, 0) + 1
+
+        for key, val in new_Map.items():
+            freq1[val].append(key)
+            # freq2[val].append(key)
+        # print(freq2)
+
+        for i in range(len(freq1) - 1, 0, -1):
+            for num in freq1[i]:
+                if len(res) == k:
+                    break
+                res.append(num)
+        
+        return res
+    
 NewSolution = Solution()
-print(NewSolution.maxArea([1,8,6,2,5,4,8,3,7]))
+print(NewSolution.topKFrequent([1,1,1,2,2,3], 2))
+
+# QUESTION
+# when i create freq like this 
+# freq1 = [[]] * (len(nums) + 1)
+# it looks like this [[], [], [], [], [], [], []]
+# the same thing goes for 
+# freq2 = [[] for i in range(len(nums) + 1)]
+# it looks like this [[], [], [], [], [], [], []]
+
+# for key, val in new_Map.items():
+#     freq1[val].append(key)
+#     freq2[val].append(key)
+
+# freq1 looks like :[[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]]
+# and freq2 look like: [[], [3], [2], [1], [], [], []]
+# Why?
