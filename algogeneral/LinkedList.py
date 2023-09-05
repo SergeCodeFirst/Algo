c6e08046d5e3ffb4ce0787d31dac6ef187137ea0
Itserge1 <sergecodefirst@gmail.com>
2023-09-05 17:18:51 -0400

    Learn linked list

--- algogeneral/LinkedList.py
+++ algogeneral/LinkedList.py
@@ -31,17 +31,61 @@ class LinkedList:
             element.append(curNode.value)
         print(element)
 
+    def extract(self,index):
+        cur_node = self.head
+        pointer = -1
+
+        if index >= self.getLength() or index < self.getLength():
+            return "Index out of range"
+        
+        while cur_node.next != None:
+            if index == pointer:
+                return cur_node.value
+            pointer += 1
+            cur_node = cur_node.next
+
+        if index == pointer:
+            return cur_node.value
+        
+    def remove(self, index):
+        cur_node = self.head
+        pointer = -1
+        preNode = cur_node
+
+        if index >= self.getLength():
+            print(f"Index out of range")
+            return self
+
+        while cur_node.next != None:
+            if pointer == index:
+                preNode.next = cur_node.next
+                cur_node.next = None
+                print(f"deleted node at index: {index}")
+                return self
+            preNode = cur_node
+            cur_node = cur_node.next
+            pointer += 1
+
+
+
+
 myList = LinkedList()
-myList.curContent()
+
+# myList.curContent()
+# print(myList.getLength())
+
 myList.addToEnd(2)
 myList.addToEnd(3)
 myList.addToEnd(4)
 myList.curContent()
+# print(myList.getLength())
 
+# print(myList.extract(2))
+# print(myList.extract(3))
 
-# myList.addToEnd(2)
-# myList.addToEnd(1)
-# myList.getLength()
-
+print(myList.remove(0).curContent())
+# print(myList.remove(1))
+# print(myList.remove(3))
+# myList.curContent()
 
 
